36c3aef95c5605ec1e6a015f6552456941bc0202
Ken42Coimbatore <101548829+csKavin@users.noreply.github.com>
2023-08-29 10:33:56 +0530

    updation

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -20,12 +20,22 @@ const useStyles = makeStyles({
         backgroundColor: 'black !important',
         color: 'white !important',
         textTransform: 'capitalize !important'
+    },
+    fixHeight: {
+        height: '400px',
+        overflowY: 'scroll'
     }
 });
 
 function Header() {
     const [data, setData] = useState([])
     const [send, setSend] = useState(0)
+    const [sendMessage, setSendMessage] = useState('')
+
+    const handleChange = (e) => {
+        // console.log(e.target.value);
+        setSendMessage(e.target.value);
+    }
     const private_key = "991d211a-0d4b-4500-9d5c-8ba5dc13f694"    //very sensitive need to store in backend
     const projectId = "2b8b731d-add1-4a1d-82f6-4d33842a509a"      //very sensitive need to store in backend
 
@@ -66,7 +76,7 @@ function Header() {
     // create Chat
     const createChat = () => {
         axios.put('https://api.chatengine.io/chats/', { "usernames": ["kavin", "kavin1"], "title": "kavin", "is_direct_chat": true },
-            { headers: { "Project-ID": projectId, "User-Name": 'kavin1', "User-Secret": "pass123" } }    //header's are very sensitive need to store in backendApi  
+            { headers: { "Project-ID": projectId, "User-Name": 'kavin', "User-Secret": "pass123" } }    //header's are very sensitive need to store in backendApi  
         )
             .then(response => {
                 console.log('User created:', response.data);
@@ -78,12 +88,14 @@ function Header() {
 
     //send message to chat
     const sendChat = () => {
-        axios.post('https://api.chatengine.io/chats/199632/messages/', { "text": "Hello Developers!" },
+        axios.post('https://api.chatengine.io/chats/199632/messages/', { "text": sendMessage },
             { headers: { "Project-ID": projectId, "User-Name": 'kavin1', "User-Secret": "pass123" } }
         )
             .then(response => {
                 console.log('User created:', response.data);
                 setSend(send + 1);
+                setSendMessage('')
+
             })
             .catch(error => {
                 console.error('Error creating user:', error);
@@ -94,7 +106,7 @@ function Header() {
     useEffect(() => {
         // const getChat = () => {
         axios.get('https://api.chatengine.io/chats/199632/messages/',
-            { headers: { "Project-ID": projectId, "User-Name": 'kavin1', "User-Secret": "pass123" } }
+            { headers: { "Project-ID": projectId, "User-Name": 'kavin', "User-Secret": "pass123" } }
         )
             .then(response => {
                 console.log('User created:', response.data);
@@ -104,7 +116,7 @@ function Header() {
                 console.error('Error creating user:', error);
             });
         // }
-    }, [send])
+    })
 
 
     // console.log(data, "data");
@@ -128,15 +140,25 @@ function Header() {
             <div className='container-fluid'>
 
                 <div className='card p-4'>
-                    {data.map((item, index) => {
-                        return <div className='d-flex justify-content between' key={index}>
-                            {item?.sender_username === "kavin1" ? <div>{item.text}</div> : <div className='d-flex flex-column align-items-end w-100'>{item.text}</div>}
-                        </div>
-                    })}
+                    <div className={classes.fixHeight}>
+                        {data.map((item, index) => {
+                            return <div className='d-flex justify-content between' key={index}>
+                                {item?.sender_username === "kavin1" ?
+                                    <div className='mt-4'>
+                                        <p>{item.created}</p>
+                                        <p className='fw-bold'>{item.text}</p >
+                                    </div> 
+                                    : <div className='d-flex flex-column align-items-end w-100 mt-4' >
+                                        <p>{item.created}</p>
+                                        <p className='fw-bold'>{item.text}</p >
+                                    </div>}
+                            </div>
+                        })}
+                    </div>
                     <br />
                     <div class="form-group">
                         <label for="exampleFormControlTextarea1" className='fw-bold'>COMPOSE MESSAGE</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" onChange={(e) => handleChange(e)}></textarea>
                     </div>
                     <div className="d-flex flex-column align-items-end mt-3">
                         <Button variant='contained' className={classes.Beginbutton1} onClick={sendChat}> send chat</Button>
